b1378e387be2b968c658ffe53cb599194c9861a3
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-11-27 23:49:33 +0100

    remaking scaffolding with vite

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders portfolio word", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/portfolio/i);
-  expect(linkElement).toBeInTheDocument();
-});
